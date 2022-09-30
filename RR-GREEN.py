00cd394d0e76188dbf07cbb9313dcaa0ebc41d2d
AFRIDI420 <107951057+AFRIDI420@users.noreply.github.com>
2022-09-30 18:17:02 +0500

    Update RR-GREEN.py

--- RR-GREEN.py
+++ RR-GREEN.py
@@ -118,8 +118,8 @@ dic = {'01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'M
 dic2 = {'01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July', '08': 'August', '09': 'September', '10': 'October', '11': 'November', '12': 'Devember'}
 tgl = datetime.datetime.now().day
 thn = datetime.datetime.now().year
-okc = 'OK-' + str(tgl) + '-' + str(bln) + '-' + str(thn) + '.txt'
-cpc = 'CP-' + str(tgl) + '-' + str(bln) + '-' + str(thn) + '.txt'
+okc = 'OK-' + str(tgl) + '-' + str(thn) + '.txt'
+cpc = 'CP-' + str(tgl) + '-' + str(thn) + '.txt'
 	
 def alvino_xy(u):
 	for e in u + '\n':
